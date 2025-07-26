f730eeb0c0a39193c31d25dd13cc3b455da9849d
Jose <146047522+jose23herrera@users.noreply.github.com>
2025-07-26 14:16:38 -0600

    Subiendo Guia#5_SEMANA5

---
+++ SEMANA_4/SEMANA5_GUIA5/src/main/java/sv/edu/udb/semana5_guia5/Semana5Guia5Application.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.semana5_guia5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana5Guia5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semana5Guia5Application.class, args);
+    }
+
+}
---
+++ SEMANA_4/SEMANA5_GUIA5/src/test/java/sv/edu/udb/semana5_guia5/Semana5Guia5ApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.semana5_guia5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana5Guia5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
