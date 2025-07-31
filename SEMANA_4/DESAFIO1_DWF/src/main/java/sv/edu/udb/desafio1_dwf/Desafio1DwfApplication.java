5de75741e0050054ea920c1f56a8e24b9f254e20
Jose <146047522+jose23herrera@users.noreply.github.com>
2025-07-31 17:23:44 -0600

    agregando archivos

---
+++ SEMANA_4/DESAFIO1_DWF/src/main/java/sv/edu/udb/desafio1_dwf/Desafio1DwfApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafio1_dwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1DwfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Desafio1DwfApplication.class, args);
+    }
+
+}
---
+++ SEMANA_4/DESAFIO1_DWF/src/test/java/sv/edu/udb/desafio1_dwf/Desafio1DwfApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafio1_dwf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1DwfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
