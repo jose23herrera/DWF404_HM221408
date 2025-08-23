adbcd447bc2bfa41076aa5a3d94df618fd244cc3
Jose <146047522+jose23herrera@users.noreply.github.com>
2025-08-22 18:10:56 -0600

    eliminando carpetas

--- Spring_guia3/src/main/java/sv/edu/udb/spring_guia3/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package sv.edu.udb.spring_guia3.service;
-
-import sv.edu.udb.spring_guia3.repository.domain.Movie;
-
-public interface MovieService {
-
-    Movie findMovieById(final Long id);
-
-}
